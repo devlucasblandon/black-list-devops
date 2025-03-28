10a5c21ced4b377e4be7a0554c861a8bc3249b83
Lucas Blandón Pulido <l.blandon@uniandes.edu.co>
2025-03-28 14:00:12 -0500

    Autorizacion con Bearer Token

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///blacklist.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-please-change' 
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-please-change'
+    API_TOKEN = os.environ.get('API_TOKEN') or 'blacklist-secret-token-2024' 
